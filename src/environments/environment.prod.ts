f8f7b4e4e7ca3726b5745abb26ec9f5c8c5347d0
YoMuchacho <moisesvilladiegos@gmail.com>
2021-03-14 20:58:22 -0500

    Primer commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '5c026cd8144e46c6814fedc02c81b787'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '5c026cd8144e46c6814fedc02c81b787'
 };
 
 /*
